93b17d92844230d4ad828003fda06bfea6fcbf39
Takeru Hayasaka <hayatake396@gmail.com>
2026-01-05 16:33:10 +0000

    feat: add config load

--- cmd/vinbero/main.go
+++ cmd/vinbero/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/takehaya/vinbero/pkg/config"
+	"github.com/urfave/cli"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
 
 func main() {
-	fmt.Println("hello")
+	app := newApp(version)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+func newApp(version string) *cli.App {
+	app := cli.NewApp()
+	app.Name = "vinbero"
+	app.Version = fmt.Sprintf("%s, %s, %s, %s", version, commit, date, builtBy)
+
+	app.Usage = "High Perfomance SRv6 Function Subset"
+
+	app.EnableBashCompletion = true
+
+	// Common flags for the main run command
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, cfg",
+			Value: "/etc/vinbero/vinbero.yaml",
+			Usage: "config path, default is /etc/vinbero/vinbero.yaml",
+		},
+	}
+	app.Action = run
+	return app
+}
+
+func run(ctx *cli.Context) error {
+	configPath := ctx.String("config")
+
+	if !config.FileExists(configPath) {
+		return fmt.Errorf("config file not found: %s", configPath)
+	}
+	c, err := config.LoadFile(configPath)
+	if err != nil {
+		return fmt.Errorf("load config error: %w", err)
+	}
+
+	_ = c // Use c or remove if not needed
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/takehaya/vinbero
 
 go 1.25.2
+
+require (
+	github.com/urfave/cli v1.22.17
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+)
